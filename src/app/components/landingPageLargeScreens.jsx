39476643ea6cfeaf203c49c7bbef8319866ff407
love-to-code108 <love.to.code108@gmail.com>
2023-10-29 23:25:38 +0530

    the component folder is functional

---
+++ src/app/components/landingPageLargeScreens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingPageLargeScreens = () => {
+  return (
+    <div>landingPageLargeScreens</div>
+  )
+}
+
+export default LandingPageLargeScreens
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import LandingPageLargeScreens from './components/landingPageLargeScreens'
 
 export default function Home() {
   return (
    <main>
     <div>Hello world</div>
+    <LandingPageLargeScreens/>
    </main> 
   )
 }
